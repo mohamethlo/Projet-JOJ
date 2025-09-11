c818348180369c8d20e8c7f021c9b184cdcf34be
MohamethLO123 <lomohameth120@gmail.com>
2025-09-11 09:53:55 +0000

    first commit

---
+++ backeend/src/main/java/sn/terangamatch/backeend/BackeendApplication.java
@@ -0,0 +1,13 @@
+package sn.terangamatch.backeend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackeendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackeendApplication.class, args);
+	}
+
+}
